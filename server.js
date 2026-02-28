a09d778030cb1f241ad7e5d7416aa7dd37c60303
nksarah2108-ai <nk.sarah2108@gmail.com>
2026-02-28 22:10:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(".")); // untuk index.html
+app.use(express.static("public")); // untuk index.html
 
 /* =====================================
    TEST ROUTE
